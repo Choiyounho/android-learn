7c762a34ea37579dbd4bff89968fd50ec469fcae
Choiyounho <maxosa78@gmail.com>
2021-01-19 17:19:40 +0900

    [learn] : Fragment

--- app/src/main/java/com/soten/androidstudio/learn/exam/openLink.kt
+++ app/src/main/java/com/soten/androidstudio/learn/exam/openLink.kt
@@ -8,12 +8,11 @@ import android.widget.Button
 import android.widget.EditText
 import com.soten.androidstudio.learn.R
 
-class onLink : AppCompatActivity() {
+class openLink : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_link)
-
-
 
         val intent4 = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/"))
         startActivity(intent4)
@@ -23,10 +22,10 @@ class onLink : AppCompatActivity() {
         val linkButton: Button = findViewById(R.id.link_button)
 
         linkButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(editText.toString()))
+            val address = editText.text
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(address.toString()))
             startActivity(intent)
         }
     }
-
 
 }
