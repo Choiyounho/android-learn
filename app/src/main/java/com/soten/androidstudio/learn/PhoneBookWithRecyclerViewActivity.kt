ad86f6fe691d291ef4bf317fc075555dec20da16
Choiyounho <maxosa78@gmail.com>
2021-01-21 13:43:39 +0900

    [learn] : RecyclerView

--- app/src/main/java/com/soten/androidstudio/learn/PhoneBookWithRecyclerViewActivity.kt
+++ app/src/main/java/com/soten/androidstudio/learn/PhoneBookWithRecyclerViewActivity.kt
@@ -1,11 +1,75 @@
 package com.soten.androidstudio.learn
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.soten.androidstudio.learn.exam.Person
+import com.soten.androidstudio.learn.exam.PhoneBook
+import com.soten.androidstudio.learn.exam.PhoneBookDetailActivity
 
 class PhoneBookWithRecyclerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone_book_with_recycler_view)
+
+        val recyclerView: RecyclerView = findViewById(R.id.phonebook_recyclerview)
+
+        with(recyclerView) {
+            this.adapter = PhoneBookRecyclerAdapter(
+                phonebookList = createFakePhoneBook(30),
+                LayoutInflater.from(this@PhoneBookWithRecyclerViewActivity),
+                activity = this@PhoneBookWithRecyclerViewActivity)
+            this.layoutManager = LinearLayoutManager(this@PhoneBookWithRecyclerViewActivity)
+        }
     }
+
+    fun createFakePhoneBook(fakeNumber: Int = 10, phoneBook: PhoneBook = PhoneBook()): PhoneBook {
+        for (i in 0 until fakeNumber) {
+            phoneBook.addPerson(Person(name = "$i 번째 사람", number = "$i 번째 번호"))
+        }
+        return phoneBook
+    }
+
+}
+
+class PhoneBookRecyclerAdapter(
+    val phonebookList: PhoneBook, val inflater: LayoutInflater,
+    val activity: Activity
+) : RecyclerView.Adapter<PhoneBookRecyclerAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val personName: TextView = itemView.findViewById(R.id.person_name)
+
+        init {
+            itemView.setOnClickListener {
+                val intent = Intent(activity, PhoneBookDetailActivity::class.java)
+
+                intent.putExtra("name", phonebookList.personList[adapterPosition].name)
+                intent.putExtra("number", phonebookList.personList[adapterPosition].number)
+
+                activity.startActivity(intent)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = inflater.inflate(R.layout.phonebook_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.personName.text = phonebookList.personList[position].name
+    }
+
+    override fun getItemCount(): Int {
+        return phonebookList.personList.size
+    }
+
 }
--- app/src/main/java/com/soten/androidstudio/learn/RecyclerViewActivity.kt
+++ app/src/main/java/com/soten/androidstudio/learn/RecyclerViewActivity.kt
@@ -1,11 +1,66 @@
 package com.soten.androidstudio.learn
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.soten.androidstudio.learn.RecyclerViewAdapter.ViewHolder
 
 class RecyclerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+
+        val carList = ArrayList<CarForList>()
+        for (i in 0 until 10) {
+            carList.add(CarForList("$i 번째 자동차", "$i 순위 엔진"))
+        }
+
+        val adapter = RecyclerViewAdapter(carList, LayoutInflater.from(this@RecyclerViewActivity))
+        val recyclerView = findViewById<RecyclerView>(R.id.phonebook_recycler_view)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this@RecyclerViewActivity)
+//        recyclerView.layoutManager = GridLayoutManager(this@RecyclerViewActivity, 2)
+
+
+    }
+}
+
+class RecyclerViewAdapter(val itemList: ArrayList<CarForList>,
+                          val inflater: LayoutInflater): RecyclerView.Adapter<ViewHolder>() {
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        val carName: TextView
+        val carEngine: TextView
+
+        init {
+            carName = itemView.findViewById(R.id.car_name)
+            carEngine = itemView.findViewById(R.id.car_engine)
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                val engineName = itemList[position].engine
+                Log.d("engine", engineName)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.item_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.carName.text = itemList[position].name
+        holder.carEngine.text = itemList[position].engine
     }
 }
