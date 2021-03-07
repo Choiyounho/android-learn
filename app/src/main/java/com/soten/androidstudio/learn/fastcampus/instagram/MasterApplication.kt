9fb1082992144b3ced3663f161770b301bd8de2a
Choiyounho <maxosa78@gmail.com>
2021-03-08 03:12:36 +0900

    [exam] : Application() 셋팅

--- app/src/main/java/com/soten/androidstudio/learn/fastcampus/instagram/MasterApplication.kt
+++ app/src/main/java/com/soten/androidstudio/learn/fastcampus/instagram/MasterApplication.kt
@@ -1,11 +1,66 @@
 package com.soten.androidstudio.learn.fastcampus.instagram
 
 import android.app.Application
+import android.content.Context
+import android.util.Log
+import com.facebook.stetho.Stetho
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.soten.androidstudio.learn.fastcampus.retrofit.RetrofitService
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MasterApplication: Application() { // Application()은 앱 전반의 설정을 할 때
+class MasterApplication : Application() { // Application()은 앱 전반의 설정을 할 때
+
+    lateinit var service: RetrofitService
 
     override fun onCreate() {
         super.onCreate()
+        Stetho.initializeWithDefaults(this@MasterApplication)
+        createRetrofit()
 
+    }
+
+    private fun createRetrofit() {
+        val header = Interceptor {
+            val original = it.request()
+            if (checkIsLogin()) {
+                getUserToken()?.let { token ->
+                    val request = original.newBuilder()
+                        .header("Authorization", token)
+                        .build()
+                    it.proceed(request)
+                }
+
+            } else {
+                it.proceed(original)
+            }
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(header)
+            .addNetworkInterceptor(StethoInterceptor())
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://melowcode.org/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        service = retrofit.create(RetrofitService::class.java)
+    }
+
+    private fun checkIsLogin(): Boolean {
+        val sharedPreferences = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
+        val token = sharedPreferences.getString("login_sp", "null")
+        token?.let { return true } ?: return false
+    }
+
+    private fun getUserToken(): String? {
+        val sharedPreferences = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
+        val token = sharedPreferences.getString("login_sp", "null")
+        token?.let { return token } ?: return null
     }
 }
--- app/src/main/java/com/soten/androidstudio/learn/fastcampus/retrofit/RetrofitService.kt
+++ app/src/main/java/com/soten/androidstudio/learn/fastcampus/retrofit/RetrofitService.kt
@@ -16,35 +16,35 @@ interface RetrofitService {
 
     @POST(value = "json/students/")
     fun createStudentEasy(@Body person : PersonFromServer) : Call<PersonFromServer>
-
-    @POST("user/signup/")
-    @FormUrlEncoded
-    fun register(
-        @Field("username") usernmae: String,
-        @Field("password1") password1: String,
-        @Field("password2") pasword2: String
-    ): Call<User>
-
-
-    @POST("user/login/")
-    @FormUrlEncoded
-    fun login(
-        @Field("username") username: String,
-        @Field("password") password: String
-    ): Call<User>
-
-
-    @GET("instagram/post/list/all/")
-    fun getAllPosts(): Call<ArrayList<Post>>
-
-    @Multipart
-    @POST("instagram/post/")
-    fun uploadPost(
-        @Part image : MultipartBody.Part,
-        @Part("content")requestBody : RequestBody
-    ):Call<Post>
-
-    @GET("instagram/post/list/")
-    fun getUserPostList():Call<ArrayList<Post>>
+//
+//    @POST("user/signup/")
+//    @FormUrlEncoded
+//    fun register(
+//        @Field("username") usernmae: String,
+//        @Field("password1") password1: String,
+//        @Field("password2") pasword2: String
+//    ): Call<User>
+//
+//
+//    @POST("user/login/")
+//    @FormUrlEncoded
+//    fun login(
+//        @Field("username") username: String,
+//        @Field("password") password: String
+//    ): Call<User>
+//
+//
+//    @GET("instagram/post/list/all/")
+//    fun getAllPosts(): Call<ArrayList<Post>>
+//
+//    @Multipart
+//    @POST("instagram/post/")
+//    fun uploadPost(
+//        @Part image : MultipartBody.Part,
+//        @Part("content")requestBody : RequestBody
+//    ):Call<Post>
+//
+//    @GET("instagram/post/list/")
+//    fun getUserPostList():Call<ArrayList<Post>>
 
 }
