8df947560f234906b491c70021f450e0901aa975
Choiyounho <maxosa78@gmail.com>
2021-03-23 00:53:36 +0900

    [learn] : Service를 이용한 데이터 전달

--- app/src/main/java/com/soten/androidstudio/learn/boostcourse/MainActivity.kt
+++ app/src/main/java/com/soten/androidstudio/learn/boostcourse/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.soten.androidstudio.learn.R
+import com.soten.androidstudio.learn.boostcourse.service.MyService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,29 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, MenuActivity::class.java)
             startActivityForResult(intent, 101) // 메뉴액티비티를 띄울 때는 101이라는 코드를 요청한다.
         }
+
+        val editText = findViewById<EditText>(R.id.et_service)
+        val serviceBtn = findViewById<Button>(R.id.btn_service)
+        serviceBtn.setOnClickListener {
+            val name = editText.text.toString()
+
+            val intent = Intent(applicationContext, MyService::class.java)
+            intent.putExtra("command", "show")
+            intent.putExtra("name", name)
+            startService(intent)
+        }
+
+        val passedIntent = intent
+        processCommand(passedIntent)
+    }
+
+    private fun processCommand(passedIntent: Intent?) {
+        passedIntent.let {
+            val command = intent.getStringExtra("command")
+            val name = intent.getStringExtra("name")
+
+            Toast.makeText(applicationContext, "서비스로부터의 응답 $command, $name", Toast.LENGTH_SHORT).show()
+        }
     }
 
     // 응답을 받아주는 메서드
@@ -27,6 +52,13 @@ class MainActivity : AppCompatActivity() {
             val name = data?.getStringExtra("name")
             Toast.makeText(applicationContext, "메뉴화면으로부터의 응답 $name", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        processCommand(intent)
+
+        super.onNewIntent(intent)
+
     }
 
 }
---
+++ app/src/main/java/com/soten/androidstudio/learn/boostcourse/service/MyService.kt
@@ -0,0 +1,55 @@
+package com.soten.androidstudio.learn.boostcourse.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import com.soten.androidstudio.learn.boostcourse.MainActivity
+
+class MyService : Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("mservice", "onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("mservice", "onDestroy")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("mservice", "onStartCommand")
+
+        intent?.let {
+            processCommand(intent)
+        } ?: return Service.START_STICKY
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun processCommand(intent: Intent?) {
+        val command = intent?.getStringExtra("command").toString()
+        val name = intent?.getStringExtra("name").toString()
+
+        Log.d("mservice", "전달받은 데이터 : $command, $name ")
+
+        try {
+            Thread.sleep(5000L)
+        } catch (e: Exception) {
+            Log.d("e", "Exception")
+        }
+
+        val showIntent = Intent(applicationContext, MainActivity::class.java)
+        showIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        showIntent.putExtra("command", "show")
+        showIntent.putExtra("name", "$name from service.")
+
+        startActivity(showIntent)
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
