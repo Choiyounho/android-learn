da81554ee998d8daedcdbfc90aeb9e6ca7a57200
Choiyounho <maxosa78@gmail.com>
2021-03-24 18:47:18 +0900

    [learn] : 브로드캐스트 문자 수신 권한 설정

--- app/src/main/java/com/soten/androidstudio/learn/boostcourse/receiver/ReceiverMainActivity.kt
+++ app/src/main/java/com/soten/androidstudio/learn/boostcourse/receiver/ReceiverMainActivity.kt
@@ -1,8 +1,5 @@
 package com.soten.androidstudio.learn.boostcourse.receiver
 
-import android.content.BroadcastReceiver
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.soten.androidstudio.learn.R
@@ -11,11 +8,5 @@ class ReceiverMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver_main)
-
-        val br : BroadcastReceiver = SmsReceiver()
-        val filter = IntentFilter().apply{
-            addAction(Intent.ACTION_SCREEN_ON)
-        }
-        registerReceiver(br, filter)
     }
 }
--- app/src/main/java/com/soten/androidstudio/learn/boostcourse/receiver/SmsReceiver.kt
+++ app/src/main/java/com/soten/androidstudio/learn/boostcourse/receiver/SmsReceiver.kt
@@ -3,15 +3,18 @@ package com.soten.androidstudio.learn.boostcourse.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.telephony.SmsMessage
 import android.util.Log
 import android.widget.Toast
 
 class SmsReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        val toast = Toast.makeText(context, "good", Toast.LENGTH_LONG)
-        toast.show()
-        Log.d("breceiver", "전송완료")
+        when(intent.action) {
+            "android.provider.Telephony.SMS_RECEIVED" -> {
+                Toast.makeText(context, "문자 받기", Toast.LENGTH_SHORT).show()
+                Log.d("breceiver", "문자 받음")
+            }
+        }
     }
+
 }
