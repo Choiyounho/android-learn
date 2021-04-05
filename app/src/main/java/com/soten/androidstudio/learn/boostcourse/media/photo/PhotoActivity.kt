1e0588e6be906e188017d79f7940aa70aa29dcd9
Choiyounho <maxosa78@gmail.com>
2021-04-05 17:58:21 +0900

    [learn] : 외부 앱으로 동영상 실행하기

--- app/src/main/java/com/soten/androidstudio/learn/boostcourse/media/photo/PhotoActivity.kt
+++ app/src/main/java/com/soten/androidstudio/learn/boostcourse/media/photo/PhotoActivity.kt
@@ -1,17 +1,35 @@
 package com.soten.androidstudio.learn.boostcourse.media.photo
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.github.chrisbanes.photoview.PhotoView
 import com.soten.androidstudio.learn.R
 
 class PhotoActivity : AppCompatActivity() {
+
+    private val editText: EditText by lazy {
+        findViewById(R.id.et_youtube)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
 
         val photoView = findViewById<PhotoView>(R.id.photoview)
-
         photoView.setImageResource(R.drawable.ic_apple)
+
+        val btn = findViewById<Button>(R.id.btn_youtube)
+        btn.setOnClickListener {
+            val url = editText.text.toString()
+
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
+
     }
 }
